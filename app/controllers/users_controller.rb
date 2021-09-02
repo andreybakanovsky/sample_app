6440b2bdb14015cf599c813ca651863b0a9ac0bb
andrey bakanovsky <andrey.bakanovsky@outlook.com>
2021-09-02 23:54:32 +0300

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,31 @@
 class UsersController < ApplicationController
-  def new
+  def show
+    @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)   # Not the final implementation!
+    # is mostly equivalent to:
+    # @user = User.new(name: "Foo Bar", email: "foo@invalid", password: "foo", password_confirmation: "bar")
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user # можно было так же redirect_to user_url(@user)
+      # Такое возможно, потому что Rails автоматически определяет, что перенаправление
+      # redirect_to @user должно быть выполнено в user_url(@user).
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      # !!! This code returns a version of the params hash with only the permitted attributes (while raising
+      # an error if the :user attribute is missing).
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
