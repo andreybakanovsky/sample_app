34c939242ec07233380fef73d2b748156750f7ee
andrey bakanovsky <andrey.bakanovsky@outlook.com>
2021-09-15 17:41:41 +0300

    Add the edit form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,21 @@ class UsersController < ApplicationController
   # def index
   #   @users = User.all
   # end
-  
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = 'Profile updated'
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   def show
     @user = User.find(params[:id])
   end
@@ -12,12 +26,12 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)   # Not the final implementation!
+    @user = User.new(user_params) # Not the final implementation!
     # params[:user] на самом деле является хэшем атрибутов пользователя
     # is mostly equivalent to: @user = User.new(name: "Foo Bar", email: "foo@invalid", password: "foo", password_confirmation: "bar")
     if @user.save
       log_in @user
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = 'Welcome to the Sample App!'
       redirect_to @user # можно было так же redirect_to user_url(@user)
       # Так3ое возможно, потому что Rails автоматически определяет, что перенаправление
       # redirect_to @user должно быть выполнено в user_url(@user).
@@ -28,9 +42,9 @@ class UsersController < ApplicationController
 
   private
 
-    def user_params
-      # !!! This code returns a version of the params hash with only the permitted attributes (while raising
-      # an error if the :user attribute is missing).
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
+  def user_params
+    # !!! This code returns a version of the params hash with only the permitted attributes (while raising
+    # an error if the :user attribute is missing).
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
