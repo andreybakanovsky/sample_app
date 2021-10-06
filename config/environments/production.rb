2608f1512e3a86cc054e2cd809e358c00ae24461
andrey bakanovsky <andrey.bakanovsky@outlook.com>
2021-10-06 14:04:37 +0300

    Change ActionMailer config, use mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,17 +66,18 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+  config.action_mailer.perform_deliveries = true # ??? tests?
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://afternoon-plains-47666.herokuapp.com'
+  host = 'afternoon-plains-47666.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    :address           => 'smtp.sendgrid.net',
+    :address           => 'smtp.mailgun.org',
     :port              => '587',
     :authentication    => :plain,
-    :user_name         => 'apikey',
-    :password          => ENV['SENDGRID_API_KEY'],
-    :domain            => 'heroku.com',
+    :user_name         => ENV['MAILGUN_SMTP_LOGIN'],
+    :password          => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain            => 'afternoon-plains-47666.herokuapp.com',
     :enable_starttls_auto => true
   }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
